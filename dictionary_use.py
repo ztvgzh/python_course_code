2960e7b6ec6b434c6bd5f3d84fed5e8aa261e8b1
Gulshad <azgulj@gmail.com>
2023-04-30 12:27:36 +0300

    new data type:dictionary

---
+++ dictionary_use.py
@@ -0,0 +1,31 @@
+def days_to_unit(number_of_days, convertion_unit):
+    if convertion_unit == "hours":
+         return f"{number_of_days} days are {number_of_days*24} hours"
+    elif convertion_unit == "minutes":
+         return f"{number_of_days} days are {number_of_days*24*60} minutes"
+    elif convertion_unit == "seconds":
+         return f"{number_of_days} days are {number_of_days*24*60*60} seconds"
+    else:
+         return "unsupported unit"
+
+
+def validate_and_execute():
+    try:
+        user_input_number = int(days_and_unit_dict["days"])
+        if user_input_number > 0:
+            calculated_value = days_to_unit(user_input_number, days_and_unit_dict["unit"])
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You intered 0, change number of days!")
+        else:
+            print("You intered negative number, change number of days!")
+    except ValueError:
+            print("Don't ruin my program!")
+
+
+user_input = ""
+while user_input != "exit":
+    user_input = input("Inter number of days and unit:\n")
+    days_and_unit = user_input.split(":")
+    days_and_unit_dict = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    validate_and_execute()
