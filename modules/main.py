69d67d8d54cc09d5d733a6979c1bdb264f538eee
Gulshad <azgulj@gmail.com>
2023-04-30 12:55:36 +0300

    using module

---
+++ modules/main.py
@@ -0,0 +1,8 @@
+import helper
+
+user_input = ""
+while user_input != "exit":
+    user_input = input("Inter number of days and unit:\n")
+    days_and_unit = user_input.split(":")
+    days_and_unit_dict = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    helper.validate_and_execute()
