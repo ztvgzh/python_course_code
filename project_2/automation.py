097daeb0bff50380b7b905d98d5b40689a2ab4b1
Gulshad <azgulj@gmail.com>
2023-05-01 14:07:38 +0300

    task3

--- project_2/automation.py
+++ project_2/automation.py
@@ -7,12 +7,13 @@ product_list = inv_file['Sheet1']
 
 profucts_per_supplier = {}
 total_value_per_supplier = {}
+products_under_10_inv = {}
 
 for product_row in range(2, product_list.max_row+1):
     supplier_name = product_list.cell(product_row, 4).value
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
-
+    product_num = int(product_list.cell(product_row, 1).value)
 
     #task 1. to find the quantity of products per supplier
     if supplier_name in profucts_per_supplier:
@@ -28,5 +29,9 @@ for product_row in range(2, product_list.max_row+1):
         total_value_per_supplier[supplier_name] = current_total_value + inventory * price
     else:
         total_value_per_supplier[supplier_name] = inventory*price
+# task 2. output: print(total_value_per_supplier)
 
-print(total_value_per_supplier)
+    #task 3. to find inventory that less 10
+    if inventory<10:
+        products_under_10_inv[product_num] = int(inventory)
+print(products_under_10_inv)
