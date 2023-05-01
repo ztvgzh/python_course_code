b84f3b7d69d100f4d93557ca2eb34acc5e3bfd5f
Gulshad <azgulj@gmail.com>
2023-05-01 13:52:46 +0300

    task2

--- project_2/automation.py
+++ project_2/automation.py
@@ -5,15 +5,28 @@ print(os.getcwd())"""
 inv_file = o.load_workbook("inventory.xlsx")
 product_list = inv_file['Sheet1']
 
-#task 1. to find the quantity of products per supplier
 profucts_per_supplier = {}
+total_value_per_supplier = {}
+
 for product_row in range(2, product_list.max_row+1):
     supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
 
+
+    #task 1. to find the quantity of products per supplier
     if supplier_name in profucts_per_supplier:
         current_num_products = profucts_per_supplier.get(supplier_name)
         profucts_per_supplier[supplier_name] = current_num_products+1
     else:
         profucts_per_supplier[supplier_name] = 1
+# task 1. output: print(profucts_per_supplier)
 
-print(profucts_per_supplier)
+    #task 2. to find total value of products for each supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory*price
+
+print(total_value_per_supplier)
