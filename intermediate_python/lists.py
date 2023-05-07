05354fd1e9dae720de3883158604812b913c7a4f
Gulshad <azgulj@gmail.com>
2023-05-07 16:37:31 +0300

    listEnd

--- intermediate_python/lists.py
+++ intermediate_python/lists.py
@@ -32,4 +32,30 @@ lst = [-5, 2, -10, 6, 8, 3, 0]
 #using sord methods
 lst.sort()
 new_lst = sorted(lst)
-print(f"{lst}\n{new_lst}")
+#print(f"{lst}\n{new_lst}")
+
+#using * to increase in ... times elements
+#print(lst*2)
+
+#using slicing
+a = lst[2:6]
+#print(a) here a sorted list 
+list_n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+b = list_n[::2] #index: 0(first index to start) 2 4 6 8: [1, 3, 5, 7, 9] 
+b2 = list_n[::3] #index: 0 3 6 9: [1, 4, 7, 10]
+#print(b)
+
+#copying to new list
+list_new = list(list_n)
+list_new1 = list_n.copy()
+# list_new2 = list_n they will use one memory 
+list_new3 = list_n[:]
+list_new.append(1)
+list_new1.append(3)
+#list_new2.append(7) 
+list_new3.append(8)
+#print(f"{list_n}\n{list_new}\n{list_new1}")
+
+c = [i*2 for i in list_n] #[i*i for i in list_n]
+print(c)
+
