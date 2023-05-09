a2e3c4e2b6bcd0d3cd0d97bf7bc4b032bd6ce261
Gulshad <azgulj@gmail.com>
2023-05-09 16:24:31 +0300

    last

---
+++ intermediate_python/dictionaries.py
@@ -0,0 +1,11 @@
+my_dict = {"name": "Gulshad", "age": 25, "city": "Nukus"}
+print(my_dict)
+my_dict2 = dict(name = "Gulshad", age = 25, city = "Nukus")
+print(my_dict2)
+
+value = my_dict["name"]
+print(value)
+
+# to add new value
+my_dict["hobby"] = "dancing"
+print(my_dict)
